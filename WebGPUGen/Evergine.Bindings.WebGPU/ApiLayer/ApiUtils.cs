47bd7901491c397f19979ed4151167b047af28cc
friflo <ullrich@praetz.de>
2025-01-24 14:05:18 +0100

    add string encoding docs

--- WebGPUGen/Evergine.Bindings.WebGPU/ApiLayer/ApiUtils.cs
+++ WebGPUGen/Evergine.Bindings.WebGPU/ApiLayer/ApiUtils.cs
@@ -2,6 +2,24 @@ namespace Evergine.Bindings.WebGPU;
 
 using System.Runtime.InteropServices;
 
+/// <summary>
+/// String encoding WGPU: "Strings are represented in UTF-8, using the WGPUStringView struct"
+/// See: https://webgpu-native.github.io/webgpu-headers/Strings.html
+/// </summary>
+/*
+Nullable value defining a pointer+length view into a UTF-8 encoded string.
+Values passed into the API may use the special length value WGPU_STRLEN to indicate a null-terminated string.
+Non-null values passed out of the API (for example as callback arguments) always provide an explicit length and may or may not be null-terminated.
+
+Some inputs to the API accept null values. Those which do not accept null values "default" to the empty string when null values are passed.
+
+Values are encoded as follows:
+- {NULL, WGPU_STRLEN}: the null value.
+- {non_null_pointer, WGPU_STRLEN}: a null-terminated string view.
+- {any, 0}: the empty string.
+- {NULL, non_zero_length}: not allowed (null dereference).
+- {non_null_pointer, non_zero_length}: an explictly-sized string view with size non_zero_length (in bytes).
+ */
 public static class ApiUtils
 {
     public static unsafe void SetArr<T>(this Span<T> src, out T* dstPtr, out ulong count) where T : unmanaged {
@@ -34,6 +52,7 @@ public static class ApiUtils
         dstPtr = null;
     }
     
+    // https://webgpu-native.github.io/webgpu-headers/Strings.html
     public static unsafe char* AllocString(this ReadOnlySpan<char> span) {
         if (span.Length == 0) {
             return null;
