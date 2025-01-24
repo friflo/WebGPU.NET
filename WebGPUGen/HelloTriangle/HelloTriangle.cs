95f834a040ea0218ecc06ed32bab53521f6fa968
friflo <ullrich@praetz.de>
2025-01-24 12:05:49 +0100

    Api - wip

--- WebGPUGen/HelloTriangle/HelloTriangle.cs
+++ WebGPUGen/HelloTriangle/HelloTriangle.cs
@@ -76,8 +76,6 @@ namespace HelloTriangle
                 hinstance = (void*)Process.GetCurrentProcess().Handle,
                 hwnd = (void*)window.Handle,
             };
-            var intPtr = new IntPtr(windowsSurface.hwnd);
-            
 
             WGPUSurfaceDescriptor surfaceDescriptor = new WGPUSurfaceDescriptor()
             {
