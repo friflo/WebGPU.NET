720806d4f698afe555cb399281c383c0163cf9d8
friflo <ullrich@praetz.de>
2025-01-24 16:37:41 +0100

    Api - object tracking

--- WebGPUGen/Evergine.Bindings.WebGPU/ApiLayer/ObjectTracker.cs
+++ WebGPUGen/Evergine.Bindings.WebGPU/ApiLayer/ObjectTracker.cs
@@ -1,20 +1,48 @@
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+
 namespace Evergine.Bindings.WebGPU;
+
+internal unsafe struct ObjectLabel
+{
+    public fixed byte buffer[64];
+}
+
+internal struct ObjectEntry
+{
+    internal ObjectLabel    label;
+    internal int            count;
+}
 
 public static class ObjectTracker
 {
-    private static Dictionary<IntPtr, int> handleMap = new Dictionary<IntPtr, int>();
+    private static readonly Dictionary<IntPtr, ObjectEntry> HandleMap = new ();
     
     public static unsafe void CreateRef(IntPtr handle, char* descriptorLabel) {
-        // var refCount = handleMap[handle];
+        HandleMap.Add(handle, new ObjectEntry { count = 1 });
     }
     
     public static void IncRef(IntPtr handle) {
-        // var refCount = handleMap[handle];
-        
+        ref var value = ref CollectionsMarshal.GetValueRefOrAddDefault(HandleMap, handle, out bool exists);
+        if (exists) {
+            value.count++;
+            return;
+        }
+        throw ObjectNotFoundException();
     }
     
     public static void DecRef(IntPtr handle) {
-        // var refCount = handleMap[handle];
-        
+        ref var value = ref CollectionsMarshal.GetValueRefOrAddDefault(HandleMap, handle, out bool exists);
+        if (exists) {
+            value.count--;
+            if (value.count > 0) {
+                return;
+            }
+            HandleMap.Remove(handle);
+            return;
+        }
+        throw ObjectNotFoundException();
     }
+    
+    private static InvalidOperationException ObjectNotFoundException() => new InvalidOperationException("WebGPU object not found");
 }
