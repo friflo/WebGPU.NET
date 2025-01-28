f5c29e74e106047db25e8b9ca1d3da42e049e659
friflo <ullrich@praetz.de>
2025-01-28 12:45:10 +0100

    add project: Tests

---
+++ WebGPUGen/Tests/Test_Arena.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+using static NUnit.Framework.Assert;
+
+namespace Tests;
+
+public static class Test_Arena
+{
+    [Test]
+    public static void Test_Arena_allocations() {
+        AreEqual(1,1);
+    }
+    
+}
