7024787dc0fc71cc0fbe27ee776dc9245dc650b7
friflo <ullrich@praetz.de>
2025-02-09 12:36:59 +0100

    wip

--- WebGPUGen/HelloTriangle-SDL/GPU.cs
+++ WebGPUGen/HelloTriangle-SDL/GPU.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.InteropServices;
 using Evergine.Bindings.WebGPU;
 using SDL;
 using static SDL.SDL3;
@@ -20,23 +21,39 @@ public class GPU
     }
     
     /// platform specific WGPU initialization
-    internal unsafe void CreateSurface(SDL_Window* window) {
+    internal unsafe void CreateSurface(SDL_Window* window)
+    {
         frameArena.Use();
-        instance = WebGPUNative.wgpuCreateInstance(new WGPUInstanceExtras {
-            backends = WGPUInstanceBackend.Vulkan
-        });
-        var properties = SDL_GetWindowProperties(window);
-        nint hinstance = SDL_GetPointerProperty(properties, SDL_PROP_WINDOW_WIN32_INSTANCE_POINTER, 0);
-        nint hwnd      = SDL_GetPointerProperty(properties, SDL_PROP_WINDOW_WIN32_HWND_POINTER,     0);
-        surface = instance.createSurfaceFromWindowsHWND(new WGPUSurfaceDescriptor(), hinstance, hwnd);
-        
-    /*  var windowWMInfo = new SDL.SDL_SysWMinfo();
-        SDL.SDL_VERSION(out windowWMInfo.version);
-        SDL.SDL_GetWindowWMInfo(window, ref windowWMInfo);
-        var hinstance   = windowWMInfo.info.win.hinstance;
-        var hwnd        = windowWMInfo.info.win.window;
-        surface = instance.createSurfaceFromWindowsHWND(new WGPUSurfaceDescriptor(), hinstance, hwnd);
-    */
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+            instance = WebGPUNative.wgpuCreateInstance(new WGPUInstanceExtras {
+                backends = WGPUInstanceBackend.Vulkan
+            });
+            var properties = SDL_GetWindowProperties(window);
+            nint hinstance = SDL_GetPointerProperty(properties, SDL_PROP_WINDOW_WIN32_INSTANCE_POINTER, 0);
+            nint hwnd      = SDL_GetPointerProperty(properties, SDL_PROP_WINDOW_WIN32_HWND_POINTER,     0);
+            surface = instance.createSurfaceFromWindowsHWND(new WGPUSurfaceDescriptor(), hinstance, hwnd);
+            
+            /*  var windowWMInfo = new SDL.SDL_SysWMinfo();
+                SDL.SDL_VERSION(out windowWMInfo.version);
+                SDL.SDL_GetWindowWMInfo(window, ref windowWMInfo);
+                var hinstance   = windowWMInfo.info.win.hinstance;
+                var hwnd        = windowWMInfo.info.win.window;
+                surface = instance.createSurfaceFromWindowsHWND(new WGPUSurfaceDescriptor(), hinstance, hwnd);
+            */
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
+            Console.WriteLine("Platform OSX untested.");
+            instance = WebGPUNative.wgpuCreateInstance(new WGPUInstanceExtras {
+                backends = WGPUInstanceBackend.Metal
+            });
+            var renderer   = SDL_CreateRenderer(window, "Renderer");
+            var metalLayer = SDL_GetRenderMetalLayer(renderer);
+            surface = instance.createSurfaceFromMetalLayer(new WGPUSurfaceDescriptor(), metalLayer);
+        }
+        else {
+            var platform = Environment.OSVersion.Platform;
+            Console.WriteLine($"Platform not implemented. platform: {platform}");
+        }
     }
 
     /// general WGPU initialization
