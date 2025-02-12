2a34b1b59fa03ae8c0ac6e5fc5952d9aac938961
friflo <ullrich@praetz.de>
2025-02-12 10:32:54 +0100

    release surface texture

--- WebGPUGen/HelloTriangle-WinForms/Program.cs
+++ WebGPUGen/HelloTriangle-WinForms/Program.cs
@@ -61,6 +61,7 @@ namespace HelloTriangle
                 
                 nextView.release();
                 surface.present();
+                surfaceTexture.texture.release();
             }
         }
     }
--- WebGPUGen/RotatingCube-SDL3/Program.cs
+++ WebGPUGen/RotatingCube-SDL3/Program.cs
@@ -69,6 +69,7 @@ namespace HelloTriangle
                 
                 nextView.release();
                 surface.present();
+                surfaceTexture.texture.release();
             }
         }
     }
