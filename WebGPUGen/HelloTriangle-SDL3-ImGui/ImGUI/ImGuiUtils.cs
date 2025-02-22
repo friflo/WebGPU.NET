97a422190174e28087a4ab217f3df3911f51759a
friflo <ullrich@praetz.de>
2025-02-22 11:11:44 +0100

    imgui_impl_sdl3.cs - compiles

--- WebGPUGen/HelloTriangle-SDL3-ImGui/ImGUI/ImGuiUtils.cs
+++ WebGPUGen/HelloTriangle-SDL3-ImGui/ImGUI/ImGuiUtils.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Text;
+using static SDL.SDL3;
 
 namespace SDLIM;
 
@@ -11,4 +13,12 @@ public static class ImGuiUtils
     }
     
     public static void IMGUI_CHECKVERSION() {}
+    
+    public static unsafe byte* SDL_strdup(string value) {
+        var len = Encoding.UTF8.GetByteCount(value);
+        var ptr = (byte*)SDL_calloc((uint)len + 1, 1);
+        Encoding.UTF8.GetBytes(value.AsSpan(), new Span<byte>(ptr, len));
+        ptr[len] = 0;
+        return ptr;
+    }
 }
